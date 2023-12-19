c289e9a87a67bf358414d35987c6151519de3642
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-19 22:49:59 +0530

    creating server and multer for file upload

--- server/server.js
+++ server/server.js
@@ -4,4 +4,11 @@ const multer = require('multer');
 const pdfparse = require('pdf-parse');
 
 const app = express();
-const PORT = process.env.Port
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongoose....', { useNewUrlParser: true, useUnifiedtopology: true });
+
+// Set up multer for file upload
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
