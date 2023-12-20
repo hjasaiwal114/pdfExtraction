cb2fe1c11693b45d5fccd68a3495ef85b161f69e
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-20 05:43:00 +0530

    adding ui/ux

--- document-processing-client/src/App.js
+++ document-processing-client/src/App.js
@@ -6,14 +6,12 @@ function App() {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (event) => {
-    // Use event.currentTarget.files to get the selected file(s)
     setSelectedFile(event.currentTarget.files[0]);
   };
 
   const handleFileUpload = async () => {
     try {
       if (!selectedFile) {
-        // Handle the case where no file is selected
         console.error('Please select a file before uploading.');
         return;
       }
@@ -21,13 +19,10 @@ function App() {
       const formData = new FormData();
       formData.append('pdf', selectedFile);
 
-      // Use relative path instead of absolute URL
-      const response = await axios.post('http://localhost:5000/upload', formData);
+      await axios.post('http://localhost:5000/upload', formData);
 
-      // Log the response from the server
-      console.log(response.data);
+      console.log('File uploaded successfully!');
     } catch (error) {
-      // Handle errors (showing error message, logging, etc.)
       console.error('Error uploading file:', error);
     }
   };
